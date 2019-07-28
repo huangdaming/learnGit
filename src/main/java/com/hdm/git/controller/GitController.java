3d0e8d2c3dbf577497902fea77ea19ae16d323df
huangdaming <861634426@qq.com>
2019-07-28 19:57:50 +0800

    Update GitController.java
    test fetch

--- src/main/java/com/hdm/git/controller/GitController.java
+++ src/main/java/com/hdm/git/controller/GitController.java
@@ -10,6 +10,6 @@ package com.hdm.git.controller;
 public class GitController {
 
     public static void main(String[] args) {
-        System.out.println("hello world  --->master second");
+        System.out.println("hello world  --->master third");
     }
 }
